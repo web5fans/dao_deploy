cb8507f4b5e4e42a852249725cce7e8cf1ff1f9e
JLer <jlerxky@live.com>
2025-10-29 10:14:26 +0800

    update

--- dao/admin.sql
+++ dao/admin.sql
@@ -1,3 +1,3 @@
 -- Auto-generated SQL script
-INSERT INTO "administrator" ("did", "permission")
+INSERT INTO public."administrator" (did, permission)
 	VALUES ('did:web5:5v5drt6o34xfhgtenepkxziy3eyuhs25', 2);
