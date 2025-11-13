b311ebabb1d6df54d5ecda022dd0d18c3f5e7865
JLer <jlerxky@live.com>
2025-11-13 10:31:16 +0800

    clean

--- apply_indexer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-envsubst < indexer/sts.yaml | kubectl -n $NAMESPACE apply -f -
-envsubst < indexer/ingress.yaml | kubectl -n $NAMESPACE apply -f -
-envsubst < indexer/svc.yaml | kubectl -n $NAMESPACE apply -f -
-envsubst < indexer/svc_node_port.yaml | kubectl -n $NAMESPACE apply -f -
--- sql_indexer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export PGPASSWORD='postgres' && psql -U postgres -h 127.0.0.1 -p $INDEXER_DB_NODE_PORT -d indexer -f indexer/init.sql
