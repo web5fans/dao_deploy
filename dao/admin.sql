f753132d8762d8a8ca747a573a141eab744b166f
JLer <jlerxky@live.com>
2025-12-26 13:48:47 +0800

    update

--- dao/admin.sql
+++ dao/admin.sql
@@ -1,3 +1,3 @@
 -- Auto-generated SQL script
-INSERT INTO public."administrator" (did, permission)
-	VALUES ('did:ckb:qgdijmd7rxlx5cimxulhxdn3cdqdrhqw', 2);
+INSERT INTO public."administrator" (did, permission) VALUES ('did:ckb:qgdijmd7rxlx5cimxulhxdn3cdqdrhqw', 2);
+INSERT INTO public."administrator" (did, permission) VALUES ('did:ckb:4q6wg6dqolkxly2tmxkypig3fn64ykq5', 2);
