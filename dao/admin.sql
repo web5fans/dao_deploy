7e9c2bea33cfc210311d20b7e946ac669a046092
JLer <jlerxky@live.com>
2026-01-31 01:40:35 +0800

    update

--- dao/admin.sql
+++ dao/admin.sql
@@ -7,3 +7,4 @@ INSERT INTO public."administrator" (did, permission) VALUES ('did:ckb:72pobge4or
 INSERT INTO public."administrator" (did, permission) VALUES ('did:ckb:5opxuzamvomeoj5f6e73hgqmb7c557do', 2);
 INSERT INTO public."administrator" (did, permission) VALUES ('did:ckb:uqpym6beyyv2p2rtknjsowbb6tjvgrhh', 2);
 INSERT INTO public."administrator" (did, permission) VALUES ('did:ckb:ez4owwwn3r7dass5xk4sj7syfjhlthgp', 2);
+INSERT INTO public."administrator" (did, permission) VALUES ('did:ckb:oaceaybwy7cwg4nwneh6lvbhrtrn5lzi', 2);
